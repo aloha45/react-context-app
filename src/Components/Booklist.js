a24403d4fe9f5962b2e8dac1cb9b53e11363e915
Cory Spicer <corys4139@gmail.com>
2021-03-09 23:20:12 -0500

    add notes of context.provider and static context

--- src/Components/Booklist.js
+++ src/Components/Booklist.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../Contexts/ThemeContext';
 
+// this method only works on class components and only one context per component
+// on the plus side--much more declarative syntax and easier to understand where the context comes from
+
 class Booklist extends Component {
+    //turn on static context
     static contextType = ThemeContext;
     render() { 
+        // pull out destructured properties from context
         const { isLightTheme, light, dark } = this.context;
+        // set those properties to a variable with a ternary
         const theme = isLightTheme ? light : dark;
         return ( 
             <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../Contexts/ThemeContext';
 
+//consumer works in functional components and class components--can consume multiple contexts within one component this way
+
 class Navbar extends Component {
-    static contextType = ThemeContext;
     render() { 
-        console.log(this.context)
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
         return ( 
-            <>
+            // instantiate context consumer and pass it the context parameter
+            <ThemeContext.Consumer>{(context) => {
+                //set consts from destructured properties received from context
+                const { isLightTheme, light, dark } = context;
+                //set consts to theme
+                const theme = isLightTheme ? light : dark;
+                return(
                 <nav style={{ background: theme.ui, color: theme.syntax }}>
                     <h1>Context App</h1>
                     <ul>
@@ -17,7 +21,9 @@ class Navbar extends Component {
                         <li>Contact</li>
                     </ul>
                 </nav>
-            </>
+                )
+            }}
+            </ThemeContext.Consumer>
          );
     }
 }
