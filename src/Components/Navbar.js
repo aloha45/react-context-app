ce22f228fa8de972fc165d19450677ba2d82d475
Cory Spicer <corys4139@gmail.com>
2021-03-11 13:21:14 -0500

    multiple context consume

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,37 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { AuthContext } from '../Contexts/AuthContext';
 import { ThemeContext } from '../Contexts/ThemeContext';
 
-//consumer works in functional components and class components--can consume multiple contexts within one component this way
-
-class Navbar extends Component {
-    render() { 
-        return ( 
-            // instantiate context consumer and pass it the context parameter
-            <AuthContext.Consumer>{(authContext) => (
-            <ThemeContext.Consumer>{(themeContext) => {
-                //set consts from destructured properties received from context
-                const { isAuthenticated, toggleAuth } = authContext;
-                const { isLightTheme, light, dark } = themeContext;
-                //set consts to theme
-                const theme = isLightTheme ? light : dark;
-                return(
-                    <nav style={{ background: theme.ui, color: theme.syntax }}>
-                    <h1>Context App</h1>
-                    <div onClick={toggleAuth}>
-                        { isAuthenticated ? 'Logged in' : 'Logged out'}
-                    </div>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
-                )
-            }}</ThemeContext.Consumer>
-            )}</AuthContext.Consumer>
-         );
-    }
+const Navbar = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+                <div onClick={toggleAuth}>
+                    { isAuthenticated ? 'Logged in' : 'Logged out'}
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+     );
 }
  
 export default Navbar;
